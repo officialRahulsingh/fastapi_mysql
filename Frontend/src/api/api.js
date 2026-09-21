09cb72de020e0b9cd01449c471b93a65ddb5e779
officialrahulsingh <rahulsinghkiddys@gmail.com>
2026-09-21 14:26:19 +0530

    update

---
+++ Frontend/src/api/api.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const API_URL = "http://13.203.0.234:8000/";
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Create Student
+export const createStudent = async (studentData) => {
+  try {
+    const response = await api.post("/students", studentData);
+    return response.data;
+  } catch (error) {
+    throw error.response?.data?.detail || "Failed to create student";
+  }
+};
+
+// Get All Students
+export const getStudents = async () => {
+  try {
+    const response = await api.get("/students");
+    return response.data;
+  } catch (error) {
+    throw error.response?.data?.detail || "Failed to fetch students";
+  }
+};
+
+// Delete Student
+export const deleteStudent = async (id) => {
+  try {
+    const response = await api.delete(`/students/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error.response?.data?.detail || "Failed to delete student";
+  }
+};
+
+export default api;
